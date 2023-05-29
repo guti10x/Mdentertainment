c4061cee6486c6ad7648577b57331331be713381
unknown <gt104515@gmail.com>
2023-05-29 16:59:07 +0200

    ventana login v1

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,98 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: Login(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
+
+class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pantalla de inicio'),
+        title: Text('Inicio de Sesión'),
       ),
-      body: const Center(
-        child: Text(
-          '¡Bienvenido a mi aplicación!',
-          style: TextStyle(fontSize: 24),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Usuario',
+              ),
+            ),
+
+            SizedBox(height: 10.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Contraseña',
+              ),
+              obscureText: true,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                // Lógica de inicio de sesión con Google
+              },
+              icon: Image.asset(
+                'assets/imagenes/gmail_logo.png',
+                width: 24.0,
+                height: 24.0,
+              ),
+              label: Text('Registrarse con Google'),
+            ),
+
+            SizedBox(height: 10.0),
+            ElevatedButton.icon(
+              onPressed: () {
+                // Login logic with Facebook
+              },
+              icon: Image.asset(
+                'assets/imagenes/facebook_logo.png',
+                width: 24.0,
+                height: 24.0,
+              ),
+              label: Text('Iniciar sesión con Facebook'),
+            ),
+
+
+            SizedBox(height: 10.0),
+            ElevatedButton.icon(
+              onPressed: () {
+                // Lógica de inicio de sesión con Apple
+              },icon: Image.asset(
+              'assets/imagenes/iPhone_logo.png',
+              width: 24.0,
+              height: 24.0,
+            ),
+              label: Text('Iniciar sesión con Apple'),
+            ),
+
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Lógica de registro
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  primary: Colors.blue,
+                  textStyle: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                child: Text(
+                  'No tienes cuenta aún, regístrate',
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
