62a490d47fde3ff020b7131d844f16a6ff82a989
unknown <gt104515@gmail.com>
2023-05-29 13:46:49 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/mdentertainmnet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mdentertainmnet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mdentertainment',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pantalla de inicio'),
+      ),
+      body: const Center(
+        child: Text(
+          '¡Bienvenido a mi aplicación!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
