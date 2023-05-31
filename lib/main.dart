871cf7a1c5491809e8c491ebcd948713abd41762
unknown <gt104515@gmail.com>
2023-05-31 17:05:37 +0200

    ventana login, registro y ventana Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,364 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mdentertainment',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Login(),
+      home: LoginPage(),
+      routes: {
+        '/registration': (context) => RegistrationPage(),
+      },
     );
   }
 }
 
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-class Login extends StatelessWidget {
+class _LoginPageState extends State<LoginPage> {
+  String username = '';
+  String password = '';
+
+  void login() {
+    // Aquí puedes agregar la lógica para verificar el usuario y la contraseña
+    if (username == 'usuario' && password == 'contrasena') {
+      // Usuario y contraseña correctos
+      // Realizar la acción deseada, como navegar a la siguiente pantalla
+    } else {
+      // Usuario o contraseña incorrectos
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Row(
+              children: [
+                Icon(
+                  Icons.error,
+                  color: Colors.red,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'Error',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+            content: Text(
+              'Usuario no encontrado',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  onPrimary: Colors.white,
+                ),
+                child: Text('Cerrar'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void goToRegistrationPage() {
+    Navigator.pushNamed(context, '/registration');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Logo de la aplicación
+              Image.asset(
+                'assets/MDentertainment.png',
+                width: 100.0,
+                height: 100.0,
+              ),
+              SizedBox(height: 20.0),
+              // Campo de entrada de usuario
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Usuario',
+                  prefixIcon: Icon(Icons.person),
+                  fillColor: Colors.grey[300],
+                  filled: true,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    username = value;
+                  });
+                },
+              ),
+              SizedBox(height: 10.0),
+              // Campo de entrada de contraseña
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  prefixIcon: Icon(Icons.lock),
+                  fillColor: Colors.grey[300],
+                  filled: true,
+                ),
+                obscureText: true,
+                onChanged: (value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+              SizedBox(height: 20.0),
+              // Botón de inicio de sesión
+              ElevatedButton(
+                onPressed: login,
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple,
+                  onPrimary: Colors.white,
+                ),
+                child: Text('Iniciar Sesión'),
+              ),
+              SizedBox(height: 30.0),
+              // Botón "No tiene cuenta aún?"
+              TextButton(
+                onPressed: goToRegistrationPage,
+                child: Container(
+                  color: Colors.grey[300],
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    'No tiene cuenta aún?',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10.0),
+              // Texto "By signing up, you agree with the [Terms of Service] and [Privacy Policy]" con corchetes rojos
+              Text(
+                'By signing up, you agree with the ',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12.0,
+                ),
+              ),
+              RichText(
+                text: TextSpan(
+                  text: '[Terms of Service]',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 12.0,
+                  ),
+                  children: [
+                    TextSpan(text: ' and '),
+                    TextSpan(
+                      text: '[Privacy Policy]',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegistrationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Inicio de Sesión'),
+        title: Text('Registrarse'),
       ),
-      body: Container(
+      body: Padding(
         padding: EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Usuario',
+            SizedBox(height: 20.0),
+            Text(
+              'Registro de usuario',
+              style: TextStyle(
+                color: Colors.purple,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline,
               ),
             ),
-
-            SizedBox(height: 10.0),
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Nombre',
+                filled: true,
+                fillColor: Colors.grey[300],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                suffixIcon: Padding(
+                  padding: EdgeInsets.only(left: 5.0),
+                  child: Icon(Icons.star, color: Colors.red, size: 14.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Primer apellido',
+                filled: true,
+                fillColor: Colors.grey[300],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                suffixIcon: Padding(
+                  padding: EdgeInsets.only(left: 5.0),
+                  child: Icon(Icons.star, color: Colors.red, size: 14.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Segundo apellido',
+                filled: true,
+                fillColor: Colors.grey[300],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Correo',
+                filled: true,
+                fillColor: Colors.grey[300],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                suffixIcon: Padding(
+                  padding: EdgeInsets.only(left: 5.0),
+                  child: Icon(Icons.star, color: Colors.red, size: 14.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
             TextField(
               decoration: InputDecoration(
                 labelText: 'Contraseña',
+                filled: true,
+                fillColor: Colors.grey[300],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                suffixIcon: Padding(
+                  padding: EdgeInsets.only(left: 5.0),
+                  child: Icon(Icons.star, color: Colors.red, size: 14.0),
+                ),
               ),
-              obscureText: true,
             ),
-            ElevatedButton.icon(
-              onPressed: () {
-                // Lógica de inicio de sesión con Google
-              },
-              icon: Image.asset(
-                'assets/imagenes/gmail_logo.png',
-                width: 24.0,
-                height: 24.0,
-              ),
-              label: Text('Registrarse con Google'),
-            ),
-
-            SizedBox(height: 10.0),
-            ElevatedButton.icon(
-              onPressed: () {
-                // Login logic with Facebook
-              },
-              icon: Image.asset(
-                'assets/imagenes/facebook_logo.png',
-                width: 24.0,
-                height: 24.0,
-              ),
-              label: Text('Iniciar sesión con Facebook'),
-            ),
-
-
-            SizedBox(height: 10.0),
-            ElevatedButton.icon(
-              onPressed: () {
-                // Lógica de inicio de sesión con Apple
-              },icon: Image.asset(
-              'assets/imagenes/iPhone_logo.png',
-              width: 24.0,
-              height: 24.0,
-            ),
-              label: Text('Iniciar sesión con Apple'),
-            ),
-
-            Container(
-              width: double.infinity,
+            SizedBox(height: 30.0),
+            SizedBox(
+              height: 40.0,
               child: ElevatedButton(
                 onPressed: () {
-                  // Lógica de registro
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => VentanaConMenu()),
+                  );
                 },
                 style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  primary: Colors.purple,
+                  onPrimary: Colors.white,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  primary: Colors.blue,
-                  textStyle: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
                 ),
                 child: Text(
-                  'No tienes cuenta aún, regístrate',
+                  'Registrarse',
+                  style: TextStyle(fontSize: 16.0),
                 ),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+
+class VentanaConMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/imagenes/map.jpg',
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              color: Colors.white,
+              height: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.home),
+                    onPressed: () {
+                      // Acción al presionar el botón "home"
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {
+                      // Acción al presionar el botón "search"
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      // Acción al presionar el botón "add"
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.person),
+                    onPressed: () {
+                      // Acción al presionar el botón "profile"
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
